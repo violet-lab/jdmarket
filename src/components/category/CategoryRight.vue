7b10af570bd02173e58fa73d5c8cd78f17c25072
wangxiaohuadegithub <1554832271@qq.com>
2018-03-08 04:54:04 +0800

    a

--- src/components/category/CategoryRight.vue
+++ src/components/category/CategoryRight.vue
@@ -9,7 +9,7 @@
       <div class="category_detail">
         <h3 class="category_n">Hot Category Goods</h3>
         <ul>
-          <li class="category_detail_item" v-for="(index, item) in cateGoodsData" :key=index>
+          <li class="category_detail_item" v-for="(item,index) in cateGoodsData" :key=index>
             <router-link :to="'/detail/'+item.product_id" class="category_detail_item_link">
               <img v-lazy="item.product_img_url" alt="" class="category_detail_item_pic">
               <p class="category_detail_item_name">{{item.product_name}}</p>
